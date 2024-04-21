6dc683b2cd4fa5a62a9e895edc813bb488cfd212
BADAVATH SRIKANTH <80474350+SRIKANTH284@users.noreply.github.com>
2024-04-21 17:58:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,6 @@ import streamlit as st
 from fpdf import FPDF
 import base64
 
-# Add the DejaVuSans font to FPDF
-def add_font(pdf):
-    # Download and unzip the font files into your project directory or specify the path
-    pdf.add_font('DejaVu', '', 'DejaVuSansCondensed.ttf', uni=True)
-
 # Center-align the components on the page
 st.set_page_config(layout="centered")
 
@@ -21,13 +16,12 @@ def create_download_link(val, filename):
 if export_as_pdf:
     pdf = FPDF()
     pdf.add_page()
-    add_font(pdf)  # Add custom font to FPDF
-    pdf.set_font('DejaVu', '', 14)  # Set font to DejaVu which supports UTF-8
+    pdf.set_font('Arial', 'B', 16)
     pdf.multi_cell(0, 10, report_text)  # Allow multi-line text in the PDF
 
-    pdf_output = pdf.output(dest="S").encode("latin-1")  # This might still need adjustment
+    pdf_output = pdf.output(dest="S").encode("latin-1")
     html = create_download_link(pdf_output, "test")
 
     st.markdown(html, unsafe_allow_html=True)
     st.success("Here you go!")  # Display success message
-    st.balloons()  # Trigger balloons
+    st.balloons()  # Trigger balloon
